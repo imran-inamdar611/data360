954526e4abaef1c4dc321cc6d5a7e7eacf38a869
imran.inamdar <imran.inamdar611@gmail.com>
2023-11-01 15:00:05 +0530

    updated

--- main.tf
+++ main.tf
@@ -15,15 +15,15 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "imran_app_rg" {
-    name = "app-grp611"
+    name = "app-grp6113"
     location = "North Europe"
   
 }
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = "imranariznew"
-  resource_group_name      = "app-grp0611"
-  location                 = "North Europe"
+  resource_group_name      = azurerm_resource_group.imran_app_rg.name
+  location                 = azurerm_resource_group.imran_app_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
   depends_on = [ azurerm_resource_group.imran_app_rg ]
