58b9137a29733fe36d10b673960ea7c816e68445
imran.inamdar <imran.inamdar611@gmail.com>
2023-11-06 10:35:39 +0530

    new2

--- main.tf
+++ main.tf
@@ -5,16 +5,6 @@ terraform {
       version = "3.77.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "app-grp611"
-    storage_account_name = "storagexxxx123"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
-    #use_azuread_auth     = true
-    subscription_id      = "24fe603e-6b8f-4384-a5c7-cc1e8f9c1a65"
-    tenant_id            = "ab8b6a07-e366-49dc-b3ca-0613f7fac7e8"
-    
-  }
 }
 provider "azurerm" {
   subscription_id = "24fe603e-6b8f-4384-a5c7-cc1e8f9c1a65"
@@ -25,13 +15,13 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "imran_app_rg" {
-    name = "app-grp123"
+    name = "app-grp1231234"
     location = "North Europe"
   
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "imranariznew1"
+  name                     = "imranew1"
   resource_group_name      = azurerm_resource_group.imran_app_rg.name
   location                 = azurerm_resource_group.imran_app_rg.location
   account_tier             = "Standard"
